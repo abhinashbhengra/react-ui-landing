896da077fcef5da23260b890c9c9411fe747f172
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-25 19:57:40 +0530

    component update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
 };
 
 export default Home;
